f70168b49562d08135280a934181b411f50bc8aa
kevin-harkin <kevin.harkin@digital.hmrc.gov.uk>
2017-11-08 15:40:51 +0000

    FSET-1811: Mandatory Library Upgrade

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -13,13 +13,9 @@ object MicroServiceBuild extends Build with MicroService {
 private object AppDependencies {
   def apply()= Seq(
     ws,
-    "uk.gov.hmrc"             %% "microservice-bootstrap"    % "5.14.0",
-    "uk.gov.hmrc"             %% "play-authorisation"        % "4.2.0",
-    "uk.gov.hmrc"             %% "play-health"               % "2.0.0",
-    "uk.gov.hmrc"             %% "play-url-binders"          % "2.0.0",
-    "uk.gov.hmrc"             %% "play-config"               % "3.0.0",
-    "uk.gov.hmrc"             %% "logback-json-logger"       % "3.0.0",
-    "uk.gov.hmrc"             %% "domain"                    % "4.0.0",
+    "uk.gov.hmrc"             %% "microservice-bootstrap"    % "6.12.0",
+    "uk.gov.hmrc"             %% "play-url-binders"          % "2.1.0",
+    "uk.gov.hmrc"             %% "domain"                    % "5.0.0",
     "uk.gov.hmrc"             %% "hmrctest"                  % "2.0.0" % "test,it",
     "org.scalatest"           %% "scalatest"                 % "2.2.6" % "test,it",
     "org.pegdown"             %  "pegdown"                   % "1.6.0" % "test,it",
